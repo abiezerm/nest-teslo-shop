0fe80bd45ac8a20bb167d71e147019ecc7bf982e
Fernando Herrera <fernando.herrera85@gmail.com>
2025-02-13 13:44:08 -0500

    Add internal server error handling to AuthService and update unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -6,7 +6,10 @@ import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto';
 import * as bcrypt from 'bcrypt';
-import { BadRequestException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -95,5 +98,30 @@ describe('AuthService', () => {
     await expect(authService.create(dto)).rejects.toThrow(
       'Email already exists',
     );
+  });
+
+  it('should throw an internal server error', async () => {
+    const dto = {
+      email: 'test@google.com',
+    } as CreateUserDto;
+
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+
+    jest
+      .spyOn(userRepository, 'save')
+      .mockRejectedValue({ code: '9999', detail: 'Unhandled error' });
+
+    await expect(authService.create(dto)).rejects.toThrow(
+      InternalServerErrorException,
+    );
+    await expect(authService.create(dto)).rejects.toThrow(
+      'Please check server logs',
+    );
+
+    expect(console.log).toHaveBeenCalledTimes(2);
+    expect(console.log).toHaveBeenCalledWith({
+      code: '9999',
+      detail: 'Unhandled error',
+    });
   });
 });
