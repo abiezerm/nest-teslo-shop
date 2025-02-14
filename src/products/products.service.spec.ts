f3be5b09e66a3c8771add2e3e32becdbe90212fb
Fernando Herrera <fernando.herrera85@gmail.com>
2025-02-14 12:25:19 -0500

    Add unit tests for ProductsService with mocked repositories

---
+++ src/products/products.service.spec.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product, ProductImage } from './entities';
+import { DataSource } from 'typeorm';
+
+describe('ProductService', () => {
+  let service: ProductsService;
+
+  beforeEach(async () => {
+    const mockProductRepository = {
+      create: jest.fn(),
+      save: jest.fn(),
+      find: jest.fn(),
+      count: jest.fn(),
+      findOneBy: jest.fn(),
+      createQueryBuilder: jest.fn(),
+      findOne: jest.fn(),
+      preload: jest.fn(),
+      remove: jest.fn(),
+    };
+
+    const mockProductImageRepository = {
+      save: jest.fn(),
+      create: jest.fn(),
+    };
+
+    const mockDataSource = {
+      createQueryRunner: jest.fn().mockReturnValue({
+        connect: jest.fn(),
+        startTransaction: jest.fn(),
+        manager: {
+          delete: jest.fn(),
+          save: jest.fn(),
+        },
+        commitTransaction: jest.fn(),
+        release: jest.fn(),
+        rollbackTransaction: jest.fn(),
+      }),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockProductRepository,
+        },
+        {
+          provide: getRepositoryToken(ProductImage),
+          useValue: mockProductImageRepository,
+        },
+        {
+          provide: DataSource,
+          useValue: mockDataSource,
+        },
+      ],
+    }).compile();
+
+    service = module.get<ProductsService>(ProductsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
