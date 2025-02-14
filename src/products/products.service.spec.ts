c89776cc8f510ddb4f56db01807bbd51dcc88dc3
Fernando Herrera <fernando.herrera85@gmail.com>
2025-02-14 14:44:48 -0500

    Add unit tests for create product functionality in ProductsService

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -3,9 +3,14 @@ import { ProductsService } from './products.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
 import { DataSource, Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { User } from '../auth/entities/user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('ProductService', () => {
   let service: ProductsService;
+  let productRepository: Repository<Product>;
+  let productImageRepository: Repository<ProductImage>;
 
   beforeEach(async () => {
     const mockProductRepository = {
@@ -58,9 +63,69 @@ describe('ProductService', () => {
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    productRepository = module.get<Repository<Product>>(
+      getRepositoryToken(Product),
+    );
+    productImageRepository = module.get<Repository<ProductImage>>(
+      getRepositoryToken(ProductImage),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a product', async () => {
+    const dto = {
+      title: 'Test Product',
+      price: 100,
+      images: ['img1.jpg'],
+    } as CreateProductDto;
+
+    const { images: dtoWithNoImages, ...createDto } = dto;
+
+    const user = {
+      id: '1',
+      email: 'test@google.com',
+    } as User;
+
+    const product = {
+      id: '1',
+      ...createDto,
+      user,
+    } as unknown as Product;
+
+    jest.spyOn(productRepository, 'create').mockReturnValue(product);
+    jest.spyOn(productRepository, 'save').mockResolvedValue(product);
+    jest
+      .spyOn(productImageRepository, 'create')
+      .mockImplementation((imageData) => imageData as unknown as ProductImage);
+
+    const result = await service.create(dto, user);
+
+    expect(result).toEqual({
+      id: '1',
+      title: 'Test Product',
+      price: 100,
+      images: ['img1.jpg'],
+      user: { id: '1', email: 'test@google.com' },
+    });
+  });
+
+  it('should throw a BadRequestException if create product fails', async () => {
+    jest.spyOn(productRepository, 'save').mockRejectedValue({
+      code: '23505',
+      detail: 'Cannot create product because XYZ',
+    });
+
+    const dto = {} as CreateProductDto;
+    const user = {} as User;
+
+    await expect(service.create(dto, user)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(service.create(dto, user)).rejects.toThrow(
+      'Cannot create product because XYZ',
+    );
   });
 });
